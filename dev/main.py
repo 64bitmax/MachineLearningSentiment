97de539658226df8aba90b5a156a872fc3b583d0
Max N <maxnorman14@googlemail.com>
2018-11-13 20:02:50 +0000

    Implementation of Naive Bayes Classifier - no save yet but new feature vector approach used

--- dev/main.py
+++ dev/main.py
@@ -1,16 +1,16 @@
 import csv
 import pickle
+import pandas
 import random
+
+from sklearn import model_selection, preprocessing, naive_bayes, metrics
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from textblob.classifiers import NaiveBayesClassifier
 
 
 class NaiveBayesTrainer:
-    tweetSentiments = []
-    trainingData = []
-    testData = []
-
-    def split_data(self, lower_bound, upper_bound):
-        self.trainingData = self.tweetSentiments[lower_bound:upper_bound]
+    tweetSentiments, tweetTexts = [], []
+    trainingData = pandas.DataFrame()
 
     def parse(self):
         path = '/Users/Max/Desktop/Machine Learning Sentiment/Sentiment Analysis Dataset.csv';
@@ -20,35 +20,39 @@ class NaiveBayesTrainer:
             for row in reader:
                 if count > 0:
                     if row[1] == '0':
-                        self.tweetSentiments.append((row[3], 'neg'))
+                        self.tweetSentiments.append('neg')
                     else:
-                        self.tweetSentiments.append((row[3], 'pos'))
+                        self.tweetSentiments.append('pos')
+                    self.tweetTexts.append(row[3])
                 count += 1
 
-    def train_model(self):
-        bayes_classifier = NaiveBayesClassifier(self.trainingData)
-        file = open('naive-bayes-classifier.pickle', 'wb')
-        pickle.dump(bayes_classifier, file)
-        file.close()
-        return classifier
+        self.tweetTexts = self.tweetTexts[0:1000]
+        self.tweetSentiments = self.tweetSentiments[0:1000]
+        self.trainingData['text'] = self.tweetTexts
+        self.trainingData['label'] = self.tweetSentiments
+
+        train_x, valid_x, train_y, valid_y = model_selection.train_test_split(self.trainingData['text'], self.trainingData['label'])
+
+        encoder = preprocessing.LabelEncoder()
+        train_y = encoder.fit_transform(train_y)
+        valid_y = encoder.fit_transform(valid_y)
+
+        return train_x, valid_x, train_y, valid_y
 
 
 if __name__ == '__main__':
     # Bayes Classifier
-    try:
-        f = open('naive-bayes-classifier.pickle', 'rb')
-        classifier = pickle.load(f)
-        f.close()
-        dist = classifier.prob_classify('School is amazing')
-        print(dist.prob('pos'))
-        print(dist.prob('neg'))
-    except(OSError, IOError, FileNotFoundError) as error:
         bayesTrainer = NaiveBayesTrainer()
-        bayesTrainer.parse()
-        bayesTrainer.split_data(0, 10000)
-        classifier = bayesTrainer.train_model()
-        dist = classifier.prob_classify('School is really bad')
-        print(dist.prob('pos'))
-        print(dist.prob('neg'))
+        train_x, valid_x, train_y, valid_y = bayesTrainer.parse()
+        count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+        count_vect.fit(bayesTrainer.trainingData['text'])
+        xtrain_count = count_vect.transform(train_x)
+        xvalid_count = count_vect.transform(valid_x)
+        multinomialNB = naive_bayes.MultinomialNB()
+        multinomialNB.fit(xtrain_count, train_y)
+        predictions = multinomialNB.predict(xvalid_count)
+        print(train_x)
+        print(predictions)
+        print(metrics.accuracy_score(predictions, valid_y))
 
     # Deep Learning Classifier
